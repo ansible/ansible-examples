0bc30b33a2c310e160090508223338b0cb5ed512
Justina Chen <rcfans@163.com>
2015-01-20 16:55:57 +0800

    environment variables

--- openstack-heat/roles/client/files/heat-initscript.sh
+++ openstack-heat/roles/client/files/heat-initscript.sh
@@ -1,4 +1,4 @@
-export OS_PASSWORD=admin_123456
+export OS_PASSWORD=password
 export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://10.74.151.172:35357/v2.0
-export OS_USERNAME=ADMIN
+export OS_AUTH_URL=http://10.0.0.1:35357/v2.0
+export OS_USERNAME=USERNAME
