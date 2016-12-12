0518db02c0910a1b28711a83d5f01680646208f8
Lee Rich <leerich@users.noreply.github.com>
2016-12-12 20:17:42 +0000

    Update enable_ansible.cmd

--- windows/testing/enable_ansible.cmd
+++ windows/testing/enable_ansible.cmd
@@ -1,1 +1,3 @@
-test
+#Comment
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1', 'C:\ConfigureRemotingForAnsible.ps1')"
+powershell -ExecutionPolicy Bypass -Command "& 'C:\ConfigureRemotingForAnsible.ps1'"
